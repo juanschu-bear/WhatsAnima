7cab8ff581ed8a79b33ade603791bd722d72218f
juanschu-bear <262013142+juanschu-bear@users.noreply.github.com>
2026-03-17 01:44:05 +0100

    Fix avatar reply module import

--- api/avatar-reply.ts
+++ api/avatar-reply.ts
@@ -5,7 +5,7 @@ import {
   prepareMessages,
   generateImageFromPrompt,
   ChatMessage,
-} from './chat'
+} from './chat.js'
 
 export default async function handler(req: any, res: any) {
   return res.status(200).json({ ok: true, step: 'chat-imports' })
