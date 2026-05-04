014603d664e13ee5a8a382ca42c35c802b07e3de
juanschu-bear <262013142+juanschu-bear@users.noreply.github.com>
2026-05-05 00:10:30 +0200

    fix: disable outbound call polling overlay

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,11 @@ import MeetingLobby from './pages/MeetingLobby'
 import MeetingHost from './pages/MeetingHost'
 import OPMPerceptionPanelPreviewScreen from './screens/OPMPerceptionPanelPreviewScreen'
 import EcosystemConsole from './pages/EcosystemConsole'
-import IncomingCallOverlay from './components/IncomingCallOverlay'
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <IncomingCallOverlay />
         <Routes>
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/login" element={<Login />} />
